d5cd56f00de99aecc8905d8a81f91104f2d5b051
nghia pham <phamhongnghia13579@gmail.com>
2024-11-16 13:38:35 +0700

    update pipeline

--- devops_cicd_java_tomcat/devops_cicd_java_tomcat_stack.py
+++ devops_cicd_java_tomcat/devops_cicd_java_tomcat_stack.py
@@ -1,19 +1,19 @@
 from ast import arg
 from aws_cdk import (
-    core,
-    aws_ec2 as ec2,
-    aws_autoscaling as autoscaling,
+    App,
+    SecretValue,
+    Stack,
     aws_elasticloadbalancingv2 as elbv2,
     aws_codepipeline as codepipeline,
     aws_codepipeline_actions as codepipeline_actions,
     aws_codebuild as codebuild,
-    aws_iam as iam
 )
-import yaml
 
-class DevopsCicdJavaTomcatStack(core.Stack):
+from constructs import Construct
 
-    def __init__(self, scope: core.Construct, id: str, environment: str, **kwargs) -> None:
+class DevopsCicdJavaTomcatStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # CodePipeline
@@ -31,7 +31,7 @@ class DevopsCicdJavaTomcatStack(core.Stack):
                     owner="phongnghia",
                     repo="devops-cicd-java-tomcat",
                     branch="main",
-                    oauth_token=core.SecretValue.secrets_manager("github-token"),
+                    oauth_token=SecretValue.secrets_manager('github-token'),
                     output=source_output,
                 )
             ]
@@ -39,7 +39,7 @@ class DevopsCicdJavaTomcatStack(core.Stack):
 
         # Build Stage
         build_project = codebuild.PipelineProject(self, "Build",
-            build_spec=codebuild.BuildSpec.from_source_filename("buildspec.yml")
+            build_spec=codebuild.BuildSpec.from_source_filename("devops_cicd_java_tomcat/buildspec.yml")
         )
 
         pipeline.add_stage(
@@ -55,13 +55,13 @@ class DevopsCicdJavaTomcatStack(core.Stack):
         )
 
         # Deploy Stage
-        pipeline.add_stage(
-            stage_name="Deploy",
-            actions=[
-                codepipeline_actions.EcsDeployAction(
-                    action_name="Deploy",
-                    service=arg,
-                    input=build_output,
-                )
-            ]
-        )
+        # pipeline.add_stage(
+        #     stage_name="Deploy",
+        #     actions=[
+        #         codepipeline_actions.EcsDeployAction(
+        #             action_name="Deploy",
+        #             service=arg,
+        #             input=build_output,
+        #         )
+        #     ]
+        # )
